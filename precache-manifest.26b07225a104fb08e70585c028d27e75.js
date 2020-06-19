e180dda92ba82b18293479788ec62a083d5e6ba6
schulreich <schulreich_daniel@peppinet.de>
2020-06-19 21:00:48 +0200

    Updates

---
+++ precache-manifest.26b07225a104fb08e70585c028d27e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdcc1ba7cd224238d9e1fa9346e49f4",
+    "url": "/project_7/index.html"
+  },
+  {
+    "revision": "ff29e7e2cceb6df43d36",
+    "url": "/project_7/static/css/main.f96f0b1c.chunk.css"
+  },
+  {
+    "revision": "9017061d7c3376817304",
+    "url": "/project_7/static/js/2.a0b1672f.chunk.js"
+  },
+  {
+    "revision": "192d54c9710402c7147477a0106b05bc",
+    "url": "/project_7/static/js/2.a0b1672f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff29e7e2cceb6df43d36",
+    "url": "/project_7/static/js/main.a4bea71b.chunk.js"
+  },
+  {
+    "revision": "e6c479fb6024ee0a9560",
+    "url": "/project_7/static/js/runtime-main.6be4dd84.js"
+  }
+]);
